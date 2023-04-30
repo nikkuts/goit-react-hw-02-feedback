eeb02848663d76be86d669dfe4a783ba46a26033
nikkuts <nickkuts888@gmail.com>
2023-04-30 13:46:15 +0300

    up

--- src/components/WidgetFeedback/WidgetFeedback.jsx
+++ src/components/WidgetFeedback/WidgetFeedback.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class WidgetFeedback extends Component {
-    static propTypes = {};
+    // static propTypes = {};
 
     state = {
         good: 0,
